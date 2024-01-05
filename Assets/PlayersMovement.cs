e8e4301ff21d750c43643562a8a2496e36f2f37a
Idealjp <155497607+Idealjp@users.noreply.github.com>
2024-01-05 01:29:15 -0800

    basic plane and player movement

---
+++ Assets/PlayersMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayersMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float movementSpeed = 500f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, movementSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -movementSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-movementSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(movementSpeed * Time.deltaTime, 0, 0);
+        }
+    }
+}
